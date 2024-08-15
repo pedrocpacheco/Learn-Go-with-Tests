53767d8102bf566d81d5d0844678f5f13a9f74cd
pedrocpacheco <cpedropacheco@hotmail.com>
2024-08-15 18:24:36 -0300

    test(hello): Hello to World in case String is empty

--- 1-Go-Fundamentals/01-hello-world/en/hello_test.go
+++ 1-Go-Fundamentals/01-hello-world/en/hello_test.go
@@ -6,10 +6,22 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Exemplo")
-	want := "Hello, Exemplo"
+	// assinatura t.Run -> func (t *T) Run(name string, f func(t *T)) bool
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Exemplo")
+		want := "Hello, Exemplo"
 
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+
 }
