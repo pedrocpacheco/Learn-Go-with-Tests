c76e18584bdee83e18cc7e08702d754d0f666a0b
pedrocpacheco <cpedropacheco@hotmail.com>
2024-08-15 11:43:09 -0300

    exclude comments

--- 1-Go-Fundamentals/01-hello-world/en/hello_test.go
+++ 1-Go-Fundamentals/01-hello-world/en/hello_test.go
@@ -6,10 +6,10 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")   // Got the return of the function
-	want := "Hello, Chris!" // Want the String "Hello World!"
+	got := Hello("Exemplo")
+	want := "Hello, Exemplo"
 
 	if got != want {
-		t.Errorf("got %q, want %q", got, want) // ! Failing -> Hello, World! != Hello World!
+		t.Errorf("got %q, want %q", got, want)
 	}
 }
