e24e27a42023bd340a517fb5d33e073b57a824c8
pedrocpacheco <cpedropacheco@hotmail.com>
2024-08-15 08:28:47 -0300

    test: if returned string is correct

---
+++ 1-Go-Fundamentals/01-hello-world/en/hello_test.go
@@ -0,0 +1,15 @@
+// ! Lets create a tes for the function Hello()
+// ? We separated our domain to better testing
+
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	got := Hello()        // Got the return of the function "Hello()"
+	want := "Hello World" // Want the String "Hello World"
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
