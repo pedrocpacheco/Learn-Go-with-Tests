0de28a114e7a691d650a722c156a19ffffbed9b8
pedrocpacheco <cpedropacheco@hotmail.com>
2024-08-15 18:35:59 -0300

    refactor(hello_test): create a helper function to failing tests

--- 1-Go-Fundamentals/01-hello-world/en/hello_test.go
+++ 1-Go-Fundamentals/01-hello-world/en/hello_test.go
@@ -10,18 +10,19 @@ func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
 		got := Hello("Exemplo")
 		want := "Hello, Exemplo"
-
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
+		assertCorrertMessage(t, got, want)
 	})
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World"
-
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
+		assertCorrertMessage(t, got, want)
 	})
+}
 
+// Created to reduce the repeted code in the tests
+func assertCorrertMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
 }
