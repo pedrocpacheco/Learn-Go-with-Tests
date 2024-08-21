9459f5b01cebd911badcc1e705d2895f92448fb4
pedrocpacheco <cpedropacheco@hotmail.com>
2024-08-21 08:57:15 -0300

    feat: example in tests

--- 1-Go-Fundamentals/02-integers/adder.go
+++ 1-Go-Fundamentals/02-integers/adder.go
@@ -1,5 +1,6 @@
 package integers
 
+// Adiciona os dois inteiros recebidos e retorna o resultado
 func Add(num1, num2 int) (result int) {
 	result = num1 + num2
 	return // retorna o valor da variavel result
--- 1-Go-Fundamentals/02-integers/adder_test.go
+++ 1-Go-Fundamentals/02-integers/adder_test.go
@@ -1,6 +1,9 @@
 package integers
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestAdd(t *testing.T) {
 	sum := Add(2, 2)
@@ -9,5 +12,10 @@ func TestAdd(t *testing.T) {
 	if sum != expected {
 		t.Errorf("Expected %d, got %d", expected, sum)
 	}
+}
 
+func ExampleAdd() {
+	soma := Add(1, 5)
+	fmt.Println(soma)
+	// Output: 6
 }
